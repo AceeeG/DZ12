df7d22feb804754169ff4c03cc950a21e6905d93
Гарифуллин Амир <amirgarifullin@gmail.com>
2023-12-10 21:30:12 +0300

    Добавьте файлы проекта.

---
+++ DZ12/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DZ12
+{
+    internal class Program
+    {
+        static void PrintNumbersFromOneToTen()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void DoExercise1()
+        {
+            Console.WriteLine("Упражнение 1\n");
+            Thread my_thread1 = new Thread(PrintNumbersFromOneToTen);
+            Thread my_thread2 = new Thread(PrintNumbersFromOneToTen);
+            Thread my_thread3 = new Thread(PrintNumbersFromOneToTen);
+
+            my_thread1.Start();
+            my_thread2.Start();
+            my_thread3.Start();
+        }
+
+        static int CalculateFactorial(int num)
+        {
+            int factorial = num;
+            for(int i = num - 1; i > 0; i--)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine($"Факториал от {num} = {factorial}");
+            return factorial;
+        }
+        static int CalculateSquare(int num)
+        {
+            Console.WriteLine($"Квадрат от {num} == {num * num}");
+            return num * num;
+        }
+        static async Task DoExercise2()
+        {
+            Console.WriteLine("Упражнение 2\n");
+
+            CalculateSquare(5);
+            Console.WriteLine("Спим 8 сек");
+            Thread.Sleep(8000);
+            await Task.Run(() => CalculateFactorial(5));
+        }
+        static void DoExercise3()
+        {
+            Console.WriteLine("Упражнение 3\nВсе методы класса\n");
+            Refl obj = new Refl();
+
+            MethodInfo[] methods = obj.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (var method in methods)
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DoExercise1();
+            Thread.Sleep(100);
+            DoExercise2();
+            DoExercise3();
+            Console.ReadKey();
+        }
+    }
+}
